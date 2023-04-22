284901b13ee0180ef6f58235dd7fd0f5b33251e4
Alastair Zotos <alastairzotos@gmail.com>
2023-04-22 19:37:55 +0800

    chore: housekeeping

--- src/state/oauth2.state.ts
+++ src/state/oauth2.state.ts
@@ -4,12 +4,10 @@ import { OAuthUserInfo } from "../models/oauth";
 import { setStorageItem, storageKeys } from "../storage";
 import { useAuthState } from "./auth.state";
 
-const setAccessToken = async (accessToken: string) => {
-  await setStorageItem(storageKeys.accessToken, accessToken);
-  useAuthState.getState().setAccessToken(accessToken);
-}
-
 export const useOAuthLogin = createQuery(
-  async (response: OAuthUserInfo) => 
-    await setAccessToken(await login(response))
+  async (response: OAuthUserInfo) => {
+    const accessToken = await login(response);
+    await setStorageItem(storageKeys.accessToken, accessToken);
+    useAuthState.getState().setAccessToken(accessToken);
+  }
 );
